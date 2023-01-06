d304cd6486404450597906cbf31977cd515020d7
unknown <piyushmangla@innotechteam.in>
2023-01-06 11:25:03 +0530

    changes in composition api

--- src/App.vue
+++ src/App.vue
@@ -4,39 +4,16 @@
 </template>
 
 <script>
-import { ref } from "vue";
 import PokemonCards from "./components/pokemonCards.vue";
-import {pokemonApi} from './components/pokemonApi.js'
+import { pokemonCompose } from "./components/pokemonApi.js";
 export default {
   name: "App",
-  setup() {
-    const pokemons = ref("");
-    const pokemonEvolve = ref("");
-
-    Promise.all([pokemonApi(1), pokemonApi(4), pokemonApi(7)]).then((data) => {
-      pokemons.value = data.map((dat) => ({
-        id: dat.id,
-        name: dat.name,
-        img: dat.sprites.other["official-artwork"].front_default,
-        types: dat.types,
-      }));
-    });
-
-    //function for evolved pokemon
-
-    const evolveState = (key) => {
-      Promise.all([pokemonApi(key + 1), pokemonApi(key + 2)]).then((data) => {
-        pokemonEvolve.value = data.map((dat) => ({
-          id: dat.id,
-          name: dat.name,
-          img: dat.sprites.other["official-artwork"].front_default,
-          types: dat.types,
-        }));
-      });
-    };
-    return { pokemons, pokemonEvolve, evolveState };
-  },
   components: { PokemonCards },
+  setup() {
+    const { pokemons, evolveState, pokemonEvolve } = pokemonCompose();
+
+    return { pokemons, evolveState, pokemonEvolve };
+  },
 };
 </script>
 
